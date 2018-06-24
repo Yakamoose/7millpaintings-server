b4cd390da2c240110da33fcd9cae2fa1818de444
Eric J <xericx.johnson@gmail.com>
2018-06-24 09:55:25 -0700

    travis issues

--- config.js
+++ config.js
@@ -5,7 +5,7 @@
 // };
 
 // exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/7-million-paintings';
-exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000'
+exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000';
 exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || 'mongodb://localhost/7-million-paintings';
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/7-million-paintings';
 exports.PORT = process.env.PORT || 8080;
